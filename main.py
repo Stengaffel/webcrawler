7528670786b9b46ad19cd6ac64beae67fd08735b
Stengaffel <alexander.w.andersson@gmail.com>
2020-09-17 21:02:02 +0200

    Added functionality to browse articles on one specific site

--- main.py
+++ main.py
@@ -1,23 +1,83 @@
+import requests
+import re
 from bs4 import BeautifulSoup
 
+# Check if the title is relevant. True if relevant, false if not
+def relevant_title(title, bad_words):
+    edited = str(title).lower()
+
+    for word in bad_words:
+        match = re.match(word, edited)
+        if match:
+            print(title)
+            print(match)
+            return False
+    
+    return True
+
 def main():
+    search_terms = ['thesis', 'ex-job', 'ex jobb', 'thesis']
 
-    html_doc = """
-    <html><head><title>The Dormouse's story</title></head>
-    <body>
-    <p class="title"><b>The Dormouse's story</b></p>
+    # Words that should not be included in the title
+    bad_words = ['phd', 'postdoc', 'professor', 'trainee']
 
-    <p class="story">Once upon a time there were three little sisters; and their names were
-    <a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
-    <a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
-    <a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
-    and they lived at the bottom of a well.</p>
+    # Set to keep track of duplicates
+    title_set = set()
 
-    <p class="story">...</p>
-    """ 
-    soup = BeautifulSoup(html_doc, 'html.parser')
+    for query in search_terms:
+        
+        indeed_url = f'https://se.indeed.com/jobs?q={query}&start=0'
 
-    print(soup.p)
+        while True:
+            html_text = requests.get(indeed_url).text
+            soup = BeautifulSoup(html_text, 'html.parser')
+
+            # Find all job-entries
+            job_entries = soup.find_all('div', class_ = 'jobsearch-SerpJobCard unifiedRow row result')
+            for div in job_entries:
+
+                # Handle title
+                title = div.h2.a.text.strip()
+
+                # Check if title has been seen before
+                if title in title_set:
+                    break
+                else:
+                    title_set.add(title)
+
+                # Check if the title contains bad words
+                if not relevant_title(title, bad_words):
+                    break
+                print(title)
+
+                # Print the company name
+                company = div.find(class_ = 'company')
+                if company != []:
+                    if company.text != None:
+                        print(company.text.strip())
+                    elif company.a.text != None:
+                        print(company.a.text.strip())
+
+                # Print the location
+                location = div.find(class_ = 'location accessible-contrast-color-location')
+                if location.text != None:
+                    print(location.text.strip())
+
+                # Print the link
+                link = div.h2.a['href']
+                print(f'https://se.indeed.com{link.strip()}')
+
+                print()
+            
+            # Go to next page if it exists
+            next_button = soup.find(attrs={'aria-label' : 'Nästa'})
+            if next_button:
+                indeed_url = f"https://se.indeed.com{next_button['href']}" 
+            else:
+                break
+            
+    print(f'{len(title_set) reults found}')
+
 
 if __name__ == '__main__':
     main()
